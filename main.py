2157438d8867bf6130720b36e8cde2f30080a6f0
prathamesh <prathameshpatil2604@gmail.com>
2024-02-07 13:40:34 +0530

    Added flipkart + beautiful soup module

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import undetected_chromedriver as webdriver
 from selenium.webdriver.common.by import By
 chrome_options = webdriver.ChromeOptions()
 from time import sleep
+from bs4 import BeautifulSoup
+
+# my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--use_subprocess")
+# options = webdriver.ChromeOptions()
+# options.add_argument(f"user-agent={my_user_agent}")
+
 
 browser = webdriver.Chrome(options=chrome_options)
 browser.get('https://www.amazon.in')
@@ -40,3 +46,65 @@ dict={}
 for i in range(len(products_price_amazon)):
     print(i,products_amazon[i],products_price_amazon[i],)
     dict[products_amazon[i]]=products_price_amazon[i]
+
+
+
+
+
+browser.get('https://www.flipkart.com')
+sleep(3)
+
+# input_search = browser.find_element(By.ID, 'twotabsearchtextbox')
+input_search_flipkart = browser.find_element(By.XPATH, "//input[@class='Pke_EE']")
+search_button_flipkart = browser.find_element(By.XPATH, "//button[@type='submit']")
+
+input_search_flipkart.send_keys("casio watches")
+sleep(1)
+search_button_flipkart.click()
+
+products_flipkart = []
+products_price_flipkart=[]
+for i in range(2):
+    print('Scraping page', i+1)
+    content=browser.page_source
+    soup=BeautifulSoup(content,features="lxml")
+    products_flipkart_all=soup.find_all('a',class_='IRpwTa')
+    products_price_flipkart_all=soup.find_all('a',class_='_30jeq3')
+    print(products_price_flipkart_all)
+    for p,j in zip(products_flipkart_all,products_price_flipkart_all):
+        products_flipkart.append(p['title'])
+        print(p['title'])
+        # products_price_flipkart.append(j['text'])
+    print(products_flipkart)
+    print(products_price_flipkart)
+    # products_flipkart = browser.find_elements(By.XPATH, "//a[@class='IRpwTa']")
+    # products_flipkart=browser.find_elements( By.CLASS_NAME, 'IRpwTa' )
+    # products_price_flipkart = browser.find_elements(By.XPATH, "//div[@class='_30jeq3']")
+    # print(products_flipkart)
+    # for p,j in zip(products_flipkart,products_price_flipkart)):
+    #     print(p[0])
+    #     # products_flipkart.append(p.title)
+    #     products_price_flipkart.append(j.title)
+    # print(products)
+    # print(products_price)
+    next_button = browser.find_element(By.XPATH, "//a[@class='_1LKTO3']")
+    href_value = next_button.get_attribute("href")
+    print(href_value)
+    # elems = WebDriverWait(webdriver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".sc-eYdvao.kvdWiq [href]")))        
+    # next_button['href'].click()
+    # sleep(4)
+
+
+
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+#
+# # Example with WebDriverWait
+# try:
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.ID, "captchacharacters"))
+#     )
+#     # Now you can interact with the element
+# except NoSuchElementException:
+#     print("Element not found")
